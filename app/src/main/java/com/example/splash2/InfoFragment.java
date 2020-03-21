2cc8f872692dc1882b946c4cb09b8c600aa50de1
Suvoo <suvodeepsinha1981@gmail.com>
2020-03-21 12:47:37 +0530

    updated info page

--- app/src/main/java/com/example/splash2/InfoFragment.java
+++ app/src/main/java/com/example/splash2/InfoFragment.java
@@ -1,9 +1,12 @@
 package com.example.splash2;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,7 +16,13 @@ public class InfoFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info,container,false) ;
+        View view = inflater.inflate(R.layout.fragment_info,container,false) ;
+
+        TextView feedback = view.findViewById(R.id.emailTxt1);
+        feedback.setText(Html.fromHtml("<a href=\"mailto:sunny.gajjar2728@gmail.com\">sunny.gajjar2728@gmail.com</a>"));
+        feedback.setMovementMethod(LinkMovementMethod.getInstance());
+
+        return view;
     }
 }
 
